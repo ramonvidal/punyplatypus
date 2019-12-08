ffb7c52d713cc76818a4321f9354dbf082f11004
Ramon Vidal <rvidal@sauer01.localdomain>
2019-12-08 15:11:45 +0100

    added celltype function - this function predicts celltype into seurat findAllMarkers result

---
+++ R/cell_annotation.R
@@ -0,0 +1,78 @@
+#' Cell type guess
+#' This function is a R adaptation from Alona similar python function. It uses PanglaoDB database
+#' @param markers Dataframe with seurat marker predictions.
+#' @param species use mouse or human. Default is mouse
+#' @import dplyr
+#' @keywords single-cell celltype
+#' @export
+#' @return A list containing celltype predictions with p-values
+#' @examples
+#' celltypes(markers, species = "mouse")
+
+
+#TODO :
+# Make p-value a parameter
+# remove the dplyr library call from here
+#
+
+#library(dplyr)
+
+cellType <- function(markers, species="mouse"){
+  markers<-markers[, c(2,6,7)]
+  markers$gene<-toupper(markers$gene)
+  split_list<- split(markers,markers$cluster)
+  all_genes_dataset<-unique(markers$gene)
+  markerSet<-markersSet(species=species)
+  markerSet_freq<-table(markerSet$official.gene.symbol)
+  weights = as.data.frame(1+sqrt(((max(markerSet_freq)-markerSet_freq)/(max(markerSet_freq)-min(markerSet_freq)))))
+
+
+  split_list_marker<- split(markerSet,markerSet$cell.type)
+  all_genes_markers<-unique(markerSet$official.gene.symbol)
+
+
+  for (i in 1:length(split_list)){
+    results=NA
+    names(results)<-"None"
+    cluster<-as.data.frame(split_list[[i]])
+    cluster<-merge(cluster, weights[weights$Var1 %in% cluster$gene,], by.x="gene", by.y="Var1")
+    for (j in 1:length(split_list_marker)){
+      markers_cellT<-as.data.frame(split_list_marker[[j]])
+      markers_cellT<-merge(markers_cellT, weights[weights$Var1 %in% markers_cellT$official.gene.symbol,], by.x="official.gene.symbol", by.y="Var1")
+      #how many expressed genesets are found in the geneset?
+      ct_exp<-(intersect(cluster$gene,markers_cellT$official.gene.symbol))
+      # how many _non_ expressed genes are found in the geneset?
+      diff_gene_dataset<-setdiff(all_genes_dataset, ct_exp)
+      ct_non_exp<-(intersect(diff_gene_dataset,markers_cellT$official.gene.symbol))
+      # how many expressed genes are NOT found in the geneset?
+      ct_exp_not_found<-setdiff(cluster$gene,markers_cellT$official.gene.symbol)
+      # how many _non_ expressed genes are NOT found in the geneset?
+      not_exp_not_found_in_geneset<-(setdiff(diff_gene_dataset, markers_cellT$official.gene.symbol))
+      odds<-fisher.test(matrix(c(length(ct_exp), length(ct_non_exp), length(ct_exp_not_found), length(not_exp_not_found_in_geneset)),  nrow = 2))
+      if(odds$p.value<0.05){ #Make this a parameter
+        #print(unique(as.character(cluster$cluster)))
+        #print(unique(as.character(markers_cellT$cell.type)))
+        #print(odds$p.value)
+        fish<-odds$p.value
+        names(fish)<-unique(as.character(markers_cellT$cell.type))
+        results<-c(results, fish)
+      }
+    }
+    print(unique(as.character(cluster$cluster)))
+    print(sort(results))
+  }
+}
+
+markersSet <- function(species="mouse"){
+  ma<-read.csv(system.file("data", "markers.tsv", package = "punyplatypus"), sep="\t")
+  if (species == 'mouse'){
+      s = 'Mm'
+    }else{
+      s = 'Hs'
+    }
+  ma<-ma[grepl(s, ma$species),]
+  ma<-ma[ma$ubiquitousness.index<0.05,]
+  ma_ss<-ma[,2:3]
+  return(ma_ss)
+}
+
